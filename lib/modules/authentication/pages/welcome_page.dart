87e844da00f096acf34bebb19cac3ef2785bed53
hiemIT <yhiemk0@gmail.com>
2022-09-01 15:24:38 +0700

    commit tao ham login gmail

--- lib/modules/authentication/pages/welcome_page.dart
+++ lib/modules/authentication/pages/welcome_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_instagram/blocs/app_state_bloc.dart';
+import 'package:social_instagram/modules/authentication/bloc/authentication_bloc.dart';
+import 'package:social_instagram/modules/authentication/enum/login_state.dart';
 import 'package:social_instagram/providers/bloc_provider.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -18,7 +20,15 @@ class _WelcomePageState extends State<WelcomePage> {
       body: Center(
         child: TextButton(
           child: const Text('Login'),
-          onPressed: _changeAppState,
+          onPressed: () async {
+            final authBloc = BlocProvider.of<AuthenticationBloc>(context);
+            final loginState = await authBloc!.loginWithGmail();
+
+            if (loginState == LoginState.success) {
+              final appStateBloc = BlocProvider.of<AppStateBloc>(context);
+              appStateBloc?.changeAppState(AppState.authorized);
+            }
+          },
         ),
       ),
     );
