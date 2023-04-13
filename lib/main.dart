c3720297a039dc6df25fe700fc30e75d94cbd1d4
Y Hiêm Kbuôr <yhiemk0@gmail.com>
2023-04-14 06:50:30 +0700

    setup awesome_notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:social_instagram/modules/splash/pages/splash_screen.dart';
+import 'package:social_instagram/utils/services/notification_service.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.initializeNotification();
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-//
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-//
-//   String _message = 'You are not sign in';
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Firebase Login'),
-//         ),
-//         body: FutureBuilder(
-//             future: _initialization,
-//             builder: (context, snapshot) {
-//               if (snapshot.hasError) {
-//                 return Text('Something went wrong');
-//               }
-//
-//               if (snapshot.connectionState == ConnectionState.done) {
-//                 return Center(
-//                   child: TextButton(
-//                     child: Text("Login"),
-//                     onPressed: signInWithGoogle,
-//                   ),
-//                 );
-//               }
-//               return CircularProgressIndicator();
-//             }));
-//   }
-//
-//   Future<UserCredential> signInWithGoogle() async {
-//     // Trigger the authentication flow
-//     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-//
-//     // Obtain the auth details from the request
-//     final GoogleSignInAuthentication? googleAuth =
-//         await googleUser?.authentication;
-//
-//     // Create a new credential
-//     final credential = GoogleAuthProvider.credential(
-//       accessToken: googleAuth?.accessToken,
-//       idToken: googleAuth?.idToken,
-//     );
-//
-//     // print get token
-//     print(credential.accessToken.toString());
-//     // Once signed in, return the UserCredential
-//     return await FirebaseAuth.instance.signInWithCredential(credential);
-//   }
-// }
