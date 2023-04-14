03bedcbb3237df4299cbe36ff889dba6ef5f8695
Y Hiêm Kbuôr <yhiemk0@gmail.com>
2023-04-14 07:06:11 +0700

    Xây chức năng Thông Báo Đẩy

---
+++ lib/modules/screens/widgets/top_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatelessWidget {
+  final String title;
+
+  const TopBar({
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 100,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).primaryColor,
+            Theme.of(context).shadowColor,
+          ],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 50, 20, 0),
+        child: Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
