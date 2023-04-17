0b5b63eaf21d4cb8e555fc0618cc581a552735f5
Y Hiêm Kbuôr <yhiemk0@gmail.com>
2023-04-18 04:25:01 +0700

    remove pub notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:social_instagram/modules/splash/pages/splash_screen.dart';
-import 'package:social_instagram/utils/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.initializeNotification();
   runApp(const App());
 }
 
